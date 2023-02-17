0ec23c717e47e2830dc6ada70cae3ba3bf73a86d
Felipe <flipeoliveira007@gmail.com>
2023-02-17 14:23:37 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// Verificação se o navegador tem suporte a API de reconhecimento de voz
+if ("webkitSpeechRecognition" in window) {
+    var recognition = new webkitSpeechRecognition();
+
+    recognition.continuous = true;
+
+    recognition.interimResults = true;
+
+    // Criando variáveis de acesso aos elementos
+    var startButton = document.getElementById("start-button");
+
+    var output = document.getElementById("output");
+    
+    // Inicia o recognition ao clicar no botáo
+    startButton.addEventListener('click', function () {
+        recognition.start();
+    })
+
+    recognition.onresult = function (event) {
+        var transcript = "";
+
+        for(var i = event.resultIndex; i < event.results.length; i++){
+            transcript += event.results[i][0].transcript;
+        }
+
+        output.innerHTML = transcript;
+
+    }
+
+    recognition.onerror = function (event) {
+        console.log('Error: ' + event.error);
+    }
+    
+} else {
+    console.log("Seu navegador não suporta a API de reconhecimento de voz!")
+}
